eb8c3f85021e8032f55bd6baf7d50174bd87b7c3
vaishnavi khandelwal <vaishnavik61199@gmail.com>
2020-09-20 22:30:49 +0530

    Initial commit

---
+++ src/components/Login.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="vue-tempalte">
+        <form>
+            <h3>Login</h3>
+
+            <div class="form-group">
+                <label>Username</label>
+                <input type="email" class="form-control form-control-lg" />
+            </div>
+
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" class="form-control form-control-lg" />
+            </div>
+
+            <button type="submit" class="btn btn-dark btn-lg btn-block">Login</button>
+
+            <p class="forgot-password text-right mt-2 mb-4">
+                <router-link to="/forgot-password">Forgot password ?</router-link>
+            </p>
+
+           <div class="social-icons">
+                <a href="#" class="fa fa-facebook"></a>
+                <a href="#" class="fa fa-google"></a>
+            </div>
+
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {}
+        }
+    }
+</script>
---
+++ src/components/Registration.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="vue-tempalte">
+        <form>
+            <h3>Registration</h3>
+
+            <div class="form-group">
+                <label>Full Name</label>
+                <input type="text" class="form-control form-control-sm"/>
+            </div>
+
+            <div class="form-group">
+                <label>Email address</label>
+                <input type="email" class="form-control form-control-sm" />
+            </div>
+
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" class="form-control form-control-sm" />
+            </div>
+            <div class="form-group">
+                <label>Mobile Number</label>
+                <input type="tel" class="form-control form-control-sm" />
+            </div>
+             <fieldset class="form-group">
+                <legend>Gender</legend>
+                <div class="form-check form-check-inline">
+                    <label class="form-check-label">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions" > Male
+                    </label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <label class="form-check-label">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions"> Female
+                    </label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <label class="form-check-label">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions"> Other
+                    </label>
+                </div>
+            </fieldset>
+
+            <div class="form-group">
+                <label>Address</label>
+                <textarea class="form-control" rows="1" id="comment"></textarea>
+            </div>
+
+            <button type="submit" class="btn btn-dark btn-lg btn-block">Register</button>
+
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {}
+        }
+    }
+</script>
